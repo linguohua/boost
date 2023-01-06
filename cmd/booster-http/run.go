6a94b4a5cc618defd0605ea672a88a9c5f30996b
linguohua <lghchinaidea@gmail.com>
2023-01-06 22:17:06 +0800

    lotus1.19: sealer config

--- cmd/booster-http/run.go
+++ cmd/booster-http/run.go
@@ -23,11 +23,13 @@ import (
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/lotus/markets/dagstore"
 	"github.com/filecoin-project/lotus/markets/sectoraccessor"
+	"github.com/filecoin-project/lotus/node/config"
 	lotus_modules "github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/repo"
 	"github.com/filecoin-project/lotus/storage/paths"
 	"github.com/filecoin-project/lotus/storage/sealer"
+	"github.com/filecoin-project/lotus/storage/sealer/storiface"
 	"github.com/ipfs/go-cid"
 	"github.com/urfave/cli/v2"
 )
@@ -151,19 +153,19 @@ var runCmd = &cli.Command{
 		}
 		defer lr.Close()
 
-		if err := lr.SetStorage(func(sc *paths.StorageConfig) {
-			sc.StoragePaths = []paths.LocalPath{}
+		if err := lr.SetStorage(func(sc *storiface.StorageConfig) {
+			sc.StoragePaths = []storiface.LocalPath{}
 		}); err != nil {
 			return fmt.Errorf("set storage config: %w", err)
 		}
 
 		// Create the store interface
 		var urls []string
-		lstor, err := paths.NewLocal(ctx, lr, storageService, urls)
+		lstor, err := paths.NewLocal(ctx, lr, storageService, urls, "", "")
 		if err != nil {
 			return fmt.Errorf("creating new local store: %w", err)
 		}
-		storage := lotus_modules.RemoteStorage(lstor, storageService, sauth, sealer.Config{
+		storage := lotus_modules.RemoteStorage(lstor, storageService, sauth, config.SealerConfig{
 			// TODO: Not sure if I need this, or any of the other fields in this struct
 			ParallelFetchLimit: 1,
 		})
